586b239eb5da553db9a2316b040f8eac1535fa4d
FridaRognstad <skyfriro@gmail.com>
2021-01-19 11:32:12 +0100

    .

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+//question 1
+const cat = {
+    complain: function () {
+        console.log("Meow!")
+    }
+}
+
+cat.complain();
