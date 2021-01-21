73018db5567c49bac7cac149d5e8ed2d465dc703
FridaRognstad <skyfriro@gmail.com>
2021-01-21 12:17:01 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,3 @@
-//question 1
-const cat = {
-    complain: function () {
-        console.log("Meow!");
-    }
-}
-
-cat.complain();
-
-//question 2
-const heading = document.querySelector("h3");
-
-heading.innerHTML = "Updated heading";
-
-//question 3
-heading.style.fontSize = "2em";
-
-//question 4
-heading.classList.add("subheading")
-
-//question 5
-const paragraphs = document.querySelectorAll("p");
-
-for (let i = 0; i < paragraphs.length; i++){
-    
-    paragraphs[i].style.color = "red";
-}
-
-//question 6
-const resultsContainer = document.querySelector(".results");
-
-resultsContainer.innerHTML = `<p>New paragraph</p>`;
-resultsContainer.style.backgroundColor = "yellow";
-
-//question 7
 const cats = [
     {
         name: "Blob",
@@ -48,6 +13,47 @@ const cats = [
 ];
 
 
+//question 1
+const cat = {
+    complain: function () {
+        console.log("Meow!");
+    }
+}
+
+cat.complain();
+
+
+//question 2
+const heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated heading";
+
+
+//question 3
+heading.style.fontSize = "2em";
+
+
+//question 4
+heading.classList.add("subheading")
+
+
+//question 5
+const paragraphs = document.querySelectorAll("p");
+
+for (let i = 0; i < paragraphs.length; i++){
+    
+    paragraphs[i].style.color = "red";
+}
+
+
+//question 6
+const resultsContainer = document.querySelector(".results");
+
+resultsContainer.innerHTML = `<p>New paragraph</p>`;
+resultsContainer.style.backgroundColor = "yellow";
+
+
+//question 7
 function listName(list) {
     for(let i = 0; i < list.length; i++){
         console.log(list[i].name);
@@ -83,6 +89,4 @@ const newHTML = createCats(cats);
 
 const catContainer = document.querySelector(".cat-container");
 
-catContainer.innerHtml = newHTML;
-
-console.log(newHTML);
+catContainer.innerHTML = newHTML;
